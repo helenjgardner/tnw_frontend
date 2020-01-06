dea338896624410771a49a37a5fa396f24ba9a48
helenjgardner <helen_gardner@hotmail.com>
2020-01-06 14:17:19 +0000

    made title a link and hidden ingredients

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -6,12 +6,18 @@ class Recipe extends React.Component {
         return (
             <div className="Recipe">
                 <div className="image-container">
-                <a href={this.props.recipe.recipeUrl} rel="noopener" target="_blank" >
+                    <a href={this.props.recipe.recipeUrl} rel="noopener" target="_blank" >
                         <img src={this.props.recipe.recipeImage} alt='' width="50" />  </a>
-                    {/* <img src={this.props.recipe.recipeImage} alt='' width="50" /> */}
                 </div>
-                <h2>{this.props.recipe.recipeTitle}</h2>
-                <div className="Recipe-information">
+                {/* helen made url a link */}
+                <div className="Recipe-Url">
+                    <a href={this.props.recipe.recipeUrl} target="_blank"> {this.props.recipe.recipeTitle} </a>
+                </div>
+                {/* old title */}
+                {/* <h2>{this.props.recipe.recipeTitle}</h2> */}
+
+                {/* Helen commented out listing ingredients */}
+                {/* <div className="Recipe-information">
                     <div className="Recipe-Ingredients">
                         <ul>{this.props.recipe.recipeIngredients.map(recipe => {
                             return <li> {recipe} </li>
@@ -21,7 +27,7 @@ class Recipe extends React.Component {
                     <div className="Recipe-Url">
                         <a href={this.props.recipe.recipeUrl}>  </a>
                     </div>
-                </div>
+                </div> */}
             </div>
         );
     }
